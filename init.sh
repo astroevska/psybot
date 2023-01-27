dda0b6cd9b6eed0baaf1f3df0fa77b5b18dfa304
annagerd <anna.kostya.volunteer@gmail.com>
2023-01-27 14:23:11 +0300

    refactor/project-structure-optimization

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ source ./env/bin/activate
 # install requirement packages
 pip install -r requirements.txt
 
-# create .env file 
+# create .env file
 echo "API_TOKEN=t123456789\nMONGODB_CONNECTION=mongodb123456789" > .env
 
 # insert token of your bot
@@ -19,4 +19,4 @@ perl -i -pe"s/t123456789/$1/g" .env
 perl -i -pe"s/mongodb123456789/$(echo $(echo $2 | sed 's/[\/&\@]/\\&/g'))/g" .env
 
 # start bot
-python main.py
+python source/main.py

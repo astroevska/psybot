3e5f01afcd234ee8d4568104b5560feeb168964b
annagerd <anna.kostya.volunteer@gmail.com>
2023-01-28 15:32:30 +0300

    test/rest-api-update-test

--- testing/fixtures/aiohttp.py
+++ testing/fixtures/aiohttp.py
@@ -9,7 +9,7 @@ from source.utils.server.helpers import postHandlerFactory
 def app(mongo_db):
     application = web.Application()
     application.router.add_post('/tests/add', postHandlerFactory(mongo_db.tests.insert_one, ["name", "description", "content", ["questions", "interpretor"]]))
-
+    application.router.add_post('/tests/update', postHandlerFactory(mongo_db.tests.update_one, ["name", "description", "content", ["questions", "interpretor"]], False, mongo_db.tests.find_one))
     return application
 
 
--- testing/tests/test_endpoints.py
+++ testing/tests/test_endpoints.py
@@ -1,13 +1,23 @@
 import pytest
+from source.constants.types import TTest
 
 from testing.fixtures.mongodb import *
 from testing.fixtures.aiohttp import *
-from testing.mocks.constants import INSERTED_TEST
+from testing.mocks.constants import INITIAL_ID, INSERTED_TEST, TEST_FOR_UPDATE
 from testing.utils import endpoint_test_handler, mongo_update_handler
 
 
 @pytest.mark.asyncio
-async def test_post_handler(cli, mongo_db):
+async def test_add_test(cli, mongo_db):
     """Is the /tests/add endpoint really push a new test to database"""
     await endpoint_test_handler(cli, "/tests/add", json=INSERTED_TEST)
     mongo_update_handler(mongo_db, INSERTED_TEST)
+
+
+@pytest.mark.asyncio
+async def test_update_test(cli, mongo_db):
+    """Is the /tests/update endpoint really update a test with new data in database"""
+    updated_test: TTest = {**TEST_FOR_UPDATE, "_id": str(INITIAL_ID), "name": "updated_test"}
+
+    await endpoint_test_handler(cli, "/tests/update", json=updated_test)
+    mongo_update_handler(mongo_db, {**TEST_FOR_UPDATE, "name": "updated_test"})
